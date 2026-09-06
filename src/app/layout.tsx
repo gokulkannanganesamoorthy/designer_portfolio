5c0e772207a8e7e54bd883a8ccb6ad44caf012fd
gokul <ggokulkannan2006@gmail.com>
2026-09-06 16:41:23 +0530

    chore: move body layout styles to globals.css to fix SSR hydration mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,7 +116,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${spaceGrotesk.variable} ${inter.variable} ${jetbrainsMono.variable} antialiased`}
-        style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}
       >
         <ThemeProvider>
           <script
